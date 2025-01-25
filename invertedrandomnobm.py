574746f7e074b6b6f39bc6adb5d190f17c245047
Malkiium <elihalldelafosse@gmail.com>
2025-01-25 16:10:40 +0100

    commit

--- invertedrandomnobm.py
+++ invertedrandomnobm.py
@@ -1,29 +1,28 @@
-import sys
 import random
 
-answer = "nop"
-lwlim = 0
-hilim = 50
-
 def randiminium():
-    global answer, lwlim, hilim
+    answer = "nop"
+    lwlim = 0
+    hilim = 50
 
     while answer.lower() != "yes":
-        print("i will try to guess!", end=". ")
+
         randnumb = random.randint(lwlim, hilim)
-        print(" is it : ", randnumb, end="?? ")
+
+        print("i will try to guess", end="! ")
+        print("is it : ", randnumb, end="?? ")
         answer = input()
+
         if answer.lower() == "lower":
-            hilim -= 1
-        if answer.lower() == "higher":
-            lwlim += 1
+            hilim = randnumb-1
+        elif answer.lower() == "higher":
+            lwlim = randnumb+1
 
     more = str = input("i found it ! hell ye ! want to try again ? ")
     if more.lower() == "yes":
         print()
-        answer = "anythingnow"
         randiminium()
     else:
         print("oki ! good bye !")
-        sys.exit()
+
 randiminium()
