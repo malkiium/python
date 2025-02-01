94c903346d891c7262e997f871ebac2b050657f9
Malkiium <elihalldelafosse@gmail.com>
2025-02-01 09:55:33 +0100

    commit

--- asciigeom.py
+++ asciigeom.py
@@ -40,19 +40,22 @@ def triangle_creux(car, hauteur):
 
 
 wha = int(input("what do you want ? : \n 1 triangle \n 2 hollow triangle \n 3 rect \n 4 hollow rect \n"))
-car = str(input("what type of char ? : "))
 
 if wha == 1:
+    car = str(input("what type of char ? : "))
     hauteur = int(input("What height for the triangle? : "))
     triangle_plein(car, hauteur)
 elif wha == 2:
+    car = str(input("what type of char ? : "))
     hauteur = int(input("What height for the hollow triangle? : "))
     triangle_creux(car, hauteur)
 elif wha == 3:
+    car = str(input("what type of char ? : "))
     longueur = int(input("What length for the rectangle? : "))
     largeur = int(input("What height for the rectangle? : "))
     rectangle_plein(car, longueur, largeur)
 elif wha == 4:
+    car = str(input("what type of char ? : "))
     longueur = int(input("What length for the hollow rectangle? : "))
     largeur = int(input("What height for the hollow rectangle? : "))
     rectangle_creux(car, longueur, largeur)
