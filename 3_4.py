de09f7650ff98dccc8a7d1bb0185178e561783fb
Malkiium <elihalldelafosse@gmail.com>
2025-02-09 18:29:06 +0100

    commit

---
+++ 3_4.py
@@ -0,0 +1,19 @@
+import random
+
+leng = random.randint(0,10)
+numbs1 = [0] * leng
+numbs2 = [0] * leng
+
+for i in range(len(numbs1)):
+    numbs1[i] = random.randint(0,10)
+    numbs2[i] = random.randint(0,10)
+numbs1.sort()
+numbs2.sort()
+
+print("numbs1 :", numbs1, "numbs2 :", numbs2)
+
+numbs3 = []
+numbs3 = numbs1 + numbs2
+numbs3.sort()
+
+print("numbs3 :", numbs3)
