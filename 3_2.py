44fd53abb1b850c5d1405b58319372df285074e3
Malkiium <elihalldelafosse@gmail.com>
2025-02-09 18:19:21 +0100

    commit

--- 3_2.py
+++ 3_2.py
@@ -1,5 +1,11 @@
-leng = list(input("what numbers do you want ? "))
-l1 = [leng]
+l1 = list(map(int, input("what numbers do you want ? ").split()))
+l2 = []
+l3 = []
 
-print(leng)
-print(l1)
+for x in range(len(l1)):
+    if l1[x]%2 == 0:
+        l2.append(l1[x])
+    else:
+        l3.append(l1[x])
+
+print(" evens :", l2, "\n odds :", l3)
