def2c1fcc6cc7e51913eb0b4d5ba483eceb7ad49
Malkiium <elihalldelafosse@gmail.com>
2025-02-09 18:31:06 +0100

    commit

--- 3_4.py
+++ 3_4.py
@@ -1,12 +1,13 @@
 import random
 
-leng = random.randint(0,10)
+leng = random.randint(0,5)
 numbs1 = [0] * leng
 numbs2 = [0] * leng
 
 for i in range(len(numbs1)):
     numbs1[i] = random.randint(0,10)
-    numbs2[i] = random.randint(0,10)
+for i in range(len(numbs2)):
+    numbs2[i] = random.randint(0, 10)
 numbs1.sort()
 numbs2.sort()
 
