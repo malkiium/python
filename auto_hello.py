40a40be23c2b703735b2bff6ff09c8baa863f496
Malkiium <elihalldelafosse@gmail.com>
2025-02-12 15:44:53 +0100

    commit

--- auto_hello.py
+++ auto_hello.py
@@ -1,5 +1,7 @@
 import webbrowser
 import time
+
+import keyboard
 import pyautogui
 
 
@@ -11,5 +13,8 @@ pyautogui.click(x=2400, y=1375)
 time.sleep(0.5)
 pyautogui.click(x=2400, y=1375)
 
-pyautogui.write("hello world", interval=0.01)
+keyboard.write("Hello World !")
+pyautogui.press("enter")
+time.sleep(1)
+keyboard.write("!lurk")
 pyautogui.press("enter")
