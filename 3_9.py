a8889e20150f29b9865917aad5398ae3ce53f7cb
Malkiium <elihalldelafosse@gmail.com>
2025-02-14 15:24:46 +0100

    commit

--- 3_9.py
+++ 3_9.py
@@ -30,3 +30,4 @@ valeurs = [34, 56, 89, 134, 152, 250, 87, -34, -187, -310]
 attendu = [34, 56, 89, 134, 150, 150, 87, -34, -150, -150]
 resultat = ecrete(valeurs, -150, 150)
 assert attendu == resultat, f"Erreur, la fonction a renvoyé {resultat}"
+hi
