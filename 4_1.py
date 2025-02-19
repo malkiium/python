bc1b4e74afe3fe9b3e0289c8c42706e90dd878b6
malkiium <elihalldelafosse@gmail.com>
2025-02-19 18:20:17 +0100

    ewo

--- 4_1.py
+++ 4_1.py
@@ -1,14 +1,28 @@
 def nearest_tens(number):
-    Tens = (number//10)*10
-    if number/10 >= Tens:
+    # Calculate the nearest multiple of 10
+    Tens = (number // 10) * 10
+    
+    # Check if the number is closer to the lower or upper multiple of 10
+    if number / 10 >= Tens:
+        # If the number is closer to the lower multiple of 10
         upperTens = Tens
-        lowerTens = Tens-10
+        lowerTens = Tens - 10
     else:
-        upperTens = Tens+10
+        # If the number is closer to the upper multiple of 10
+        upperTens = Tens + 10
         lowerTens = Tens
-    print(lowerTens, upperTens)
-    return (lowerTens, upperTens)
+    
+    # Create a tuple with the nearest lower and upper multiples of 10
+    nears = (lowerTens, upperTens)
+    
+    # Print the tuple of nearest tens
+    print(nears)
+    
+    # Return the tuple of nearest tens
+    return nears
 
-
+# Ask the user to input a number
 n = int(input("give a number : "))
+
+# Call the function with the user's number
 nearest_tens(n)
