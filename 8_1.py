be1f145334b1295fb9f8e402f1bf524b27a2d4d4
malkiium <elihalldelafosse@gmail.com>
2025-02-20 18:14:47 +0100

    feat: remove unnecessary comments from integer input function

--- 8_1.py
+++ 8_1.py
@@ -1,8 +1,3 @@
-"""Ecrire un programme permettant de saisir un entier. Si l’utilisateur saisit autre chose qu’un entier,
-afficher un message d’erreur en gérant l’exception ValueError. Le programme doit boucler
-jusqu’à ce que l’utilisateur saisisse bien un entier."""
-
-
 def saisir_entier():
     try:
         entier = int(input("Saisissez un entier: "))
