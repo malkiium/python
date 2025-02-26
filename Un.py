18839c161fcf58e9f947ca3e5bc4bf4a108b5fe4
malkiium <elihalldelafosse@gmail.com>
2025-02-26 15:09:46 +0100

    feat: add Un.py for mathematical computation with error handling

---
+++ Un.py
@@ -0,0 +1,12 @@
+import math
+
+Un = -5/3
+Un1 = 0
+
+for i in range(100):
+    try:
+        Un1 = 1 / (1 + Un)
+        Un = (round(Un1, 2))
+    except ZeroDivisionError:
+        print(f"   1 \n ----- \n 1{Un} \n\n this is a Zero Division. \n number Un+{i}")
+        break
