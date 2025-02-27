75320834119b4bfa1c93a797e9bc522c7609c630
malkiium <elihalldelafosse@gmail.com>
2025-02-27 10:20:59 +0100

    test: add assertions and display for construireTabCoeff function in exo3_12.py

--- exo3_12.py
+++ exo3_12.py
@@ -14,6 +14,34 @@ def affichage(tab: list):
         print(" ".join(map(str, ligne)))
 
 
-tab =[[1], [1, 1]]
-construireTabCoeff(tab, 23)
+tab = [[1], [1, 1]]
+assert construireTabCoeff(tab,2) == [[1], [1, 1]]
+tab = [[1], [1, 1]]
+assert construireTabCoeff(tab,3) == [[1], [1, 1], [1,2,1]]
+tab = [[1], [1, 1]]
+assert construireTabCoeff(tab,5) == [[1], [1, 1], [1,2,1], [1, 3, 3, 1], [1, 4, 6, 4, 1]]
+
+print("---------------------------------------")
+tab = [[1], [1, 1]]
+construireTabCoeff(tab,2)
+# 1
+# 1 1
+affichage(tab)
+
+print("---------------------------------------")
+tab = [[1], [1, 1]]
+construireTabCoeff(tab,3)
+# 1
+# 1 1
+# 1 2 1
+affichage(tab)
+
+print("---------------------------------------")
+tab = [[1], [1, 1]]
+construireTabCoeff(tab,5)
+# 1
+# 1 1
+# 1 2 1
+# 1 3 3 1
+# 1 4 6 4 1
 affichage(tab)
