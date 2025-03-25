19471b9efa2ce2678445968335e86398e5baa592
malkiium <elihalldelafosse@gmail.com>
2025-03-25 03:31:43 +0100

    feat: add checkerboard pattern generator in exo1_4_2.py

---
+++ exos/exo1/exo1_4_2.py
@@ -0,0 +1,9 @@
+taille = int(input("quelle est la taille du damier ? : "))
+
+for i in range(taille):
+    for j in range(taille):
+        if (i + j) % 2 == 0:
+            print("X", end=" ")
+        else:
+            print("O", end=" ")
+    print()
