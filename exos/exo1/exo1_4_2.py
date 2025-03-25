e349e9d6ca4337867890249f8bd0e753c3db45a8
malkiium <elihalldelafosse@gmail.com>
2025-03-25 03:33:26 +0100

    refactor: simplify checkerboard pattern generation logic in exo1_4_2.py

--- exos/exo1/exo1_4_2.py
+++ exos/exo1/exo1_4_2.py
@@ -2,7 +2,7 @@ taille = int(input("quelle est la taille du damier ? : "))
 
 for i in range(taille):
     for j in range(taille):
-        if (i + j) % 2 == 0:
+        if (i+j) % 2 == 0:
             print("X", end=" ")
         else:
             print("O", end=" ")
