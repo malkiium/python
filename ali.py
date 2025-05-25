ba0e083817579ec5260ddcb29500dc91490bf815
malkiium <elihalldelafosse@gmail.com>
2025-05-25 22:17:35 +0200

    feat: enhance ali.py with additional list operations, set operations, and dictionary manipulations

--- ali.py
+++ ali.py
@@ -51,4 +51,52 @@ liste1.extend(liste2)
 print(liste1)
 liste1.reverse()
 print(liste1)
-liste1 = 
+liste1 = liste1[::-1]
+print(liste1)
+liste1.insert(4,69)
+liste1.insert(2,70)
+print(liste1)
+liste1 = liste1[::-2]
+print(liste1)
+listeuwu = list(sorted(liste1))
+print(listeuwu)
+
+
+
+
+insta = ["insta", 245]
+allistaire = ["telegram", insta, "tiktok"]
+tuple1 = tuple(allistaire)
+allistaire.append("snapchat")
+insta.append("instagram")
+print(tuple1)
+
+
+print("\n\n\n\n\n\n")
+
+E = {1, 2, 3, 4, 5}
+F = {4, 5, 6, 7, 8}
+print(E & F)  # Intersection
+print(E - F)
+print(F - E)  # Difference
+print(E | F)  # Union
+print(E ^ F)  # Symmetric difference
+print((E | F) - (E & F))  # Union minus intersection
+
+print("\n\n\n\n\n\n")
+
+alli = {
+    "name": "Ali",
+    "age": 20,
+    "city": "Paris",
+    "hobbies": ["reading", "gaming", "coding"],
+}
+
+alli["salaire"] = 2000
+alli["age"] = 19
+del alli["age"]
+
+
+
+for key, value in alli.items():
+    print(f"{key}: {value}")
