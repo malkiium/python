f93b56dc978da073c263cfb246f99777cc3bea8c
malkiium <elihalldelafosse@gmail.com>
2025-09-17 22:15:46 +0200

    feat: add sine wave graph plotting to Waves scene

--- manimstuff/trial.py
+++ manimstuff/trial.py
@@ -3,3 +3,8 @@ import numpy as np
 
 class Waves(Scene):
     def construct(self):
+        a = Axes(x_range=[-1, 10], y_range=[-1, 10])
+
+        graph = a.plot(lambda x: np.sin(x), color=BLUE)
+
+        self.add(graph, a)
