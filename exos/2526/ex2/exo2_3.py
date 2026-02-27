acbd9f903fc0c11ee701f8c57d7f2adf652f0015
malkiium <elihalldelafosse@gmail.com>
2026-02-27 10:58:14 +0100

    exo2_3 fait

--- exos/2526/ex2/exo2_3.py
+++ exos/2526/ex2/exo2_3.py
@@ -1,22 +1,22 @@
 def anagram(word1 : str, word2 : str) -> bool:
     word1, word2 = list(word1), list(word2)
     print(word1, word2)
-    dif = len(word1)-1
 
     if len(word1) != len(word2):
-        return(-1)
+        return(False)
     else:
         for i in range(len(word1)):
-            for j in range(i, len(word2)):
+            for j in range(len(word2)):
                 if word1[i] == word2[j]:
-                    dif -=1
-        dif -= 1
-        if dif > 0:
-            print("ce ne sont pas des anagrames.")
-            print(dif)
-            return(False)
-        else:
-            print("ce sont des anagrames.")
-            return(True)
+                    word1[i] = None
+                    word2[j] = None
+                    break
+        for i in range(len(word1)):
+            if word1[i] != None:
+                print("ce ne sont pas des anagrames.")
+                print(word1)
+                return(False)
+        print("ce sont des anagrames.")
+        return(True)
 
 anagram("chine", "niche")
