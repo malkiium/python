080e3502bb7dae68e30d47cfacb6cd7687612ec4
malkiium <elihalldelafosse@gmail.com>
2026-03-30 00:09:47 +0200

    lecacaaaa

--- simpleprojects/iterations/collatz/collatzconj.py
+++ simpleprojects/iterations/collatz/collatzconj.py
@@ -1,12 +1,23 @@
 x = 1
 strs = 1
+iters = 0
+maxiters = 0
+nmbmaxiters = 0
 
-while True:
-    print(x, strs)
+while strs<1000000:
+    iters+=1
+    print(x, strs, iters)
     if x%2 == 0:
         x = x//2
     elif x == 1:
+        if iters > maxiters:
+            maxiters = iters
+            nmbmaxiters = strs
         strs+=1
         x = strs
+        iters = 0
     else:
         x = x*3 +1
+
+
+print("max iters:", maxiters, "for number:", nmbmaxiters)
